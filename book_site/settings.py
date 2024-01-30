a60cadb58894111b010ba58c0463d68d08809c3b
Abdul Khaleq <abdulkhaleq6008@gmail.com>
2024-01-30 12:59:34 +0600

    added

--- book_site/settings.py
+++ book_site/settings.py
@@ -19,11 +19,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://post-2-drf.onrender.com','https://*.127.0.0.1']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
